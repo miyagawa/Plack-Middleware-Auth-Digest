6bb60e8aa8596086448af0518ea799c762c84d12
xaicron <xaicron@gmail.com>
2010-01-07 05:08:40 +0900

    initial commit

---
+++ lib/Plack/Middleware/Auth/Digest.pm
@@ -0,0 +1,41 @@
+package Plack::Middleware::Auth::Digest;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Auth::Digest -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Auth::Digest;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Auth::Digest is
+
+=head1 METHOD
+
+=over
+
+=item
+
+=back
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
